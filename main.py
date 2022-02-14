32c4442de01a56d7b344021b113605699147d8fa
Angela L L <98534270+achen153@users.noreply.github.com>
2022-02-14 17:19:48 +0000

    corrections second

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import random
 
-my_list = ["strawberry", "blueberry", "raspberry", "blackberry", "cranberry"]
-print(random.choice(my_list))
-
-
 #Part A
-weeks = 16
-classes = 5
-tuition = 6000
-cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week, type(cost_per_week))
-
-
-#Part B
 weeks = 16
 classes = 3
 tuition = 6000
@@ -20,10 +8,11 @@ cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week, type(cost_per_week))
 
 classes_per_week = 3
-print("Class per week:", classes_per_week, type(classes_per_week))
+print("Classes per week:", classes_per_week, type(classes_per_week))
 
 cost_per_class = cost_per_week / classes_per_week
 print("Cost per class", cost_per_class, type(cost_per_class))
 
-cost_per_week = 125
-print("Cost per week:", cost_per_week, type(cost_per_class))
+#Part B
+my_list = ["strawberry", "blueberry", "raspberry", "blackberry", "cranberry"]
+print("Random berry is", random.choice(my_list))
